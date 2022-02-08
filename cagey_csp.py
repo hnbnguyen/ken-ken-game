fc8f99f8603d8c0dcd699175adc87de7546d9478
Kaitlyn Hung <kaitlynhung@Kaitlyns-MacBook-Pro.local>
2022-02-08 14:30:52 -0500

    Added student name, group ID, and date

--- cagey_csp.py
+++ cagey_csp.py
@@ -1,7 +1,7 @@
 # =============================
-# Student Names:
-# Group ID:
-# Date:
+# Student Names: Kaitlyn Hung  
+# Group ID: 80
+# Date: 02/08/2022
 # =============================
 # CISC 352 - W22
 # cagey_csp.py
