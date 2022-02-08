e5cec0c23ec0d52ccf6e0b1753634540344c1a34
hannbnguyen <baohanmimi@gmail.com>
2022-02-08 15:13:25 -0500

    adding 2nd 400

--- cagey_csp.py
+++ cagey_csp.py
@@ -1,7 +1,7 @@
 # =============================
 # Student Names: Kaitlyn Hung, Ngoc Bao Han Nguyen, Jude Tear
 
-# Group ID: 80 + 400
+# Group ID: 80 + 400 + 400
 # Date: 02/08/2022
 # =============================
 # CISC 352 - W22
