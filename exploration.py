8dded92271487b4569851d59ae5b2c7f6d3d49c8
hannbnguyen <baohanmimi@gmail.com>
2022-02-13 16:41:55 -0500

    adding exploration changes

--- exploration.py
+++ exploration.py
@@ -15,17 +15,26 @@ for r in range(1, grid_size + 1) :
     variables.append(cur_row)
 
 constraints =[]
-for r in range(grid_size):
-    for c in range(grid_size):
-        for n in range(c + 1, grid_size):
-            row_constraint = Constraint(name = "Cons(Var({},{}) and Var({},{}))".format(r + 1, c + 1, r + 1, n + 1), \
-                scope= [variables[r][c], variables[r][n]])
+# for r in range(grid_size):
+#     for c in range(grid_size):
+#         for n in range(c + 1, grid_size):
+#             row_constraint = Constraint(name = "Cons(Var({},{}) and Var({},{}))".format(r + 1, c + 1, r + 1, n + 1), \
+#                 scope= [variables[r][c], variables[r][n]])
             
-            col_constraint = Constraint(name = "Cons(Var({},{}) and Var({},{}))".format(r + 1, c + 1, n + 1, r + 1), \
-                scope = [variables[c][r], variables[n][r]])
+#             col_constraint = Constraint(name = "Cons(Var({},{}) and Var({},{}))".format(r + 1, c + 1, n + 1, r + 1), \
+#                 scope = [variables[c][r], variables[n][r]])
 
-            constraints.append(row_constraint)
-            constraints.append(col_constraint)
-            
+#             constraints.append(["Cons1", row_constraint])
+#             constraints.append(["Cons2", col_constraint])
+for r in range(0, grid_size):
+    for c in range(0, grid_size):
+        for a in range(c + 1, grid_size):
+            con1 = Constraint("C(V{}{},V{}{})".format(r+1, c+1, r+1, a+1),\
+                                [variables[r][c], variables[r][a]])
+            con2 = Constraint("C(V{}{},V{}{})".format(c+1, r+1, a+1, r+1),\
+                                [variables[c][r], variables[a][r]])
+            constraints.append(["Cons1", con1])
+            constraints.append(["Cons2", con2])
+
 for constraint in constraints:
-    print(constraint)
+    print(constraint[0], constraint[1])
